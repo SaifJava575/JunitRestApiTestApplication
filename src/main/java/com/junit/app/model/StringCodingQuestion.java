fe72c7644c69e6f67754433d3e9609371462d370
Saifuddin575408 <sksaifuddin575@gmail.com>
2023-05-02 10:59:37 +0530

    Test the functional REQUIREMENT

--- src/main/java/com/junit/app/model/StringCodingQuestion.java
+++ src/main/java/com/junit/app/model/StringCodingQuestion.java
@@ -67,4 +67,35 @@ public class StringCodingQuestion {
 		}
 		return -1;
 	}
+	
+	public static String modify(String s) {
+		int start = 0;
+		int end = s.length() - 1;
+		char ch[] = s.toCharArray();
+		while (start < end) {
+			if (!isVowel(ch[start])) {
+				start++;
+				continue;
+			}
+			if (!isVowel(ch[end])) {
+				end--;
+				continue;
+			}
+			char c = ch[start];
+			ch[start] = ch[end];
+			ch[end] = c;
+
+			start++;
+			end--;
+		}
+		return String.valueOf(ch);
+	}
+
+	public static boolean isVowel(char c) {
+		if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O'
+				|| c == 'U')
+			return true;
+
+		return false;
+	}
 }
--- src/test/java/com/junit/app/test/StringQuestionTestFunction.java
+++ src/test/java/com/junit/app/test/StringQuestionTestFunction.java
@@ -9,58 +9,69 @@ import com.junit.app.model.StringCodingQuestion;
 
 public class StringQuestionTestFunction {
 
-	
 	@Test
 	@DisplayName("Roatate String Test Function")
 	public void rotateStringTest() {
-		boolean res=StringCodingQuestion.isRotated("amazon", "azonam");
+		boolean res = StringCodingQuestion.isRotated("amazon", "azonam");
 		Assert.assertTrue(res);
 	}
-	
+
 	@Test
 	@DisplayName("Addition Test Methods")
 	public void addTest() {
-		Assert.assertEquals(30,JunitFunctionDeclaration.add(10, 20));
+		Assert.assertEquals(30, JunitFunctionDeclaration.add(10, 20));
 	}
-	
+
 	@Test
 	@DisplayName(" Longest Common Substring Test methods")
 	public void testLongestCommonSubstring() {
-		Assert.assertEquals(4, JunitFunctionDeclaration.longestCommonSubstr("ABCDGH", "ACDGHR"));	
+		Assert.assertEquals(4, JunitFunctionDeclaration.longestCommonSubstr("ABCDGH", "ACDGHR"));
 	}
-	 @Test
-	 @DisplayName("IP Check Test Api Function")
+
+	@Test
+	@DisplayName("IP Check Test Api Function")
 	public void ipCheckTest() {
-		boolean res=JunitFunctionDeclaration.checkIp("0.0.34.0");
+		boolean res = JunitFunctionDeclaration.checkIp("0.0.34.0");
 		Assert.assertTrue(res);
 		Assert.assertTrue("Valid Ip Address", res);
 	}
-	 
-	 @Test
-	 @DisplayName("Remove Duplicate Adjacent Character")
-	 public void removeDuplicateTest() {
-		 String msg=JunitFunctionDeclaration.removeDuplicate("Saifuddin");
-		 Assert.assertEquals("Saifuin", msg);
-	 }
-	 
-	 @Test
-	 @DisplayName("Non repeating charater Junit Testing Methods")
-	 public void nonRepeatingChar() {
-		 String actualRes=StringCodingQuestion.nonRepeatingCharater("hellow");
-		 Assert.assertEquals("h", actualRes);
-	 }
-	 
-	 @Test
-	 @DisplayName("Multiply Two String methods test")
-	 public void multiplyTest() {
-		 String res=StringCodingQuestion.multiplyTwoString("33", "10");
-		 Assert.assertEquals("330", res);
-	 }
-	 
-	 @Test
-	 @DisplayName("Implement Two String strstr function test")
-	 public void strstrTest() {
-		 int res=StringCodingQuestion.strstr("GeeksForGeeks", "For");
-		 Assert.assertEquals(5, res);
-	 }
+
+	@Test
+	@DisplayName("Remove Duplicate Adjacent Character")
+	public void removeDuplicateTest() {
+		String msg = JunitFunctionDeclaration.removeDuplicate("Saifuddin");
+		Assert.assertEquals("Saifuin", msg);
+	}
+
+	@Test
+	@DisplayName("Non repeating charater Junit Testing Methods")
+	public void nonRepeatingChar() {
+		String actualRes = StringCodingQuestion.nonRepeatingCharater("hellow");
+		Assert.assertEquals("h", actualRes);
+	}
+
+	@Test
+	@DisplayName("Multiply Two String methods test")
+	public void multiplyTest() {
+		String res = StringCodingQuestion.multiplyTwoString("33", "10");
+		Assert.assertEquals("330", res);
+	}
+
+	@Test
+	@DisplayName("Implement Two String strstr function test")
+	public void strstrTest() {
+		int res = StringCodingQuestion.strstr("GeeksForGeeks", "For");
+		Assert.assertEquals(5, res);
+	}
+
+	@Test
+	@DisplayName("Test The vowel reverse of String")
+	public void vowelReverseOfString() {
+		Assert.assertEquals("Sehol", StringCodingQuestion.modify("Sohel"));
+		Assert.assertEquals("Siufiddan", StringCodingQuestion.modify("Saifuddin"));
+		Assert.assertNotEquals("Saifiddan", StringCodingQuestion.modify("Saifuddin"));
+		Assert.assertNotSame("Sujan", StringCodingQuestion.modify("Saifuddin"));
+		Assert.assertNotNull(StringCodingQuestion.modify("Sujan"));
+		
+	}
 }
